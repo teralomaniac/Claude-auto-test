dca5cbc6ec4515f52b8bc50061e1d74248be2800
teralomaniac <steins_skyobserver@outlook.com>
2023-12-18 15:28:25 +0800

    0.2

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
 let Config = {
     apiKey: ['sk-ant-api...'],  //填入'sk-ant-api...','sk-ant-api...'...
-    api_rProxy: '',             //填入api镜像地址，留空则使用官网
+    api_rProxy: '',             //（可选）填入api镜像地址，留空则使用官网
     count: 0,                   //发送次数，0为不停止
-    max_tokens: 1000,           //最大响应长度
+    genPrompt: 0,               //生成提示词，0为关闭，正整数为每请求几次中生成一次提示词
+    max_tokens: 4000,           //最大响应长度
     model: 'claude-2.1',        //模型名
     interval: [100,300],        //时间间隔，填入随机范围，例如[100,300]为100到300秒随机
+    temperature: 0              //温度
 };
 
 const fs = require('fs');
 
-const getPrompt = () => {
+const getPrompt = path => {
     let data, prompts;
     try {
-        data = fs.readFileSync('prompts.txt', 'utf8');
+        data = fs.readFileSync(path, 'utf8');
     } catch (err) {
         console.error('Read Error: ', err);
         process.exit();
     }
     prompts = data.split(/\n+/g);
-    return prompts[Math.floor(Math.random() * prompts.length)].replace(/(\r\n|\r|\\n)/gm, '\n');
+    return prompts[Math.floor(Math.random() * prompts.length)].trim().replace(/(\r\n|\r|\\n)/gm, '\n');
+}, writePrompts = (path, prompts) => {
+    fs.appendFile(path, prompts, err => {
+        err && console.error(err);
+    });
 }, randomInterval = () => {
     const [min, max] = Config.interval;
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 !async function() {
-    Config.apiKey = [...new Set(Config.apiKey.join('').match(/sk-ant-api\d\d-[\w-]{86}-[\w-]{6}AA/g))];
+    Config.apiKey = [...new Set([Config.apiKey].join('').match(/sk-ant-api\d\d-[\w-]{86}-[\w-]{6}AA/g))];
     for (let i = 0; i < Config.count || Config.count === 0; i++) {
-        const prompt = getPrompt();
-        console.log(`Count: ${i + 1}\nReq: ${prompt}`);
+        const genPrompt = Config.genPrompt && ((i + 1) % Config.genPrompt === 0);
+        const prompt = getPrompt(genPrompt ? 'genprompts.txt' : 'prompts.txt');
+        console.log(`Count: ${i + 1}${genPrompt ? ' (gen)' : ''}\nReq: ${prompt}`);
         await fetch(`${Config.api_rProxy ? Config.api_rProxy : 'https://api.anthropic.com'}/v1/complete`, {
             method: 'POST',
             headers: {
@@ -39,12 +46,17 @@ const getPrompt = () => {
             body: JSON.stringify({
                 model: Config.model,
                 max_tokens_to_sample: Config.max_tokens,
-                prompt: '\n\nHuman: ' + prompt + '\n\nAssistant: '
+                temperature: Config.temperature,
+                prompt: prompt
+                    .replace(/(?:^.*?)(\n\nHuman:|)/s, function(match, p1) {return !match.includes('\n\nAssistant:') && p1 != '' ? match : '\n\nHuman: ' + match})
+                    .replace(/(\n\nAssistant:|)(?:.*?$)/s, function(match, p1) {return !match.includes('\n\nHuman:') && p1 != '' ? match :  match + '\n\nAssistant: '})
             }),
         }).then(response => response.json())
-            .then(result => console.log(`Res:${result.completion || JSON.stringify(result)}\n`))
+            .then(result => {
+                console.log(`Res:${result.completion || JSON.stringify(result)}\n`);
+                genPrompt && writePrompts('prompts.txt', result.completion.replace(/^ *\d+\. */gm,''));
+            })
             .catch(err => console.error('Req Error: ', err));
         await new Promise(resolve => setTimeout(resolve, randomInterval()*1000));
     }
 }();
-
--- readme.md
+++ readme.md
@@ -1,6 +1,11 @@
 # Claude Auto Test / Claude自动测试
-使用方法：
+基础使用：
   - 利用GPT等AI生成问题集
   - 将问题集以换行分隔放入prompts.txt，其中问题中如果本身包含换行则用\n代替
   - 配置app.js中的Config项，例如apiKey等
   - 使用start.bat或命令行node app.js启动
+
+进阶使用：
+  - 编写用于生成问题集的提示词集，注意必须添加\n\nHuman:和\n\nAssistant:，并且建议为Assistant写入开头避免开头被写入prompts（例: \n\nHuman:生成十个问题\n\nAssistant:以下是十个问题:\n）
+  - 在genPrompts中填写用于生成问题集的提示词集，其中换行用\n代替
+  - Config中的genPrompts项设置为正整数，值代表请求多少次生成一次提示词
